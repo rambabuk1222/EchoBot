2d25f6d6a4897a81c5eb53ef3fde38fad73f859e
ram <rambabuk1222@gmail.com>
2019-11-23 21:53:30 +0000

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+            var config = SpeechConfig.FromSubscription("----------------------------", "northeurope");
+            using (var recognizer = new SpeechRecognizer(config, AudioConfig.FromDefaultMicrophoneInput()))
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: Please some words."), cancellationToken);
+                var result=  await recognizer.RecognizeOnceAsync();
+
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {result.Text}"), cancellationToken);
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome!"), cancellationToken);
+                }
+            }
+        }
+    }
+}
